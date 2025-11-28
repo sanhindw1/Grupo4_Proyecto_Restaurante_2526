c70b060bebb3dd31068032a380127b3dbf73ca04
Sandra Hinojosa <sanhindw@gmail.com>
2025-11-28 16:38:58 +0100

    Cambios en Conexion y modificación adicional U1

--- src/oraclegeneral/Conexion.java
+++ src/oraclegeneral/Conexion.java
@@ -106,7 +106,7 @@ public class Conexion {
      */
     public static Boolean creacionUsuario() {
         JOptionPane.showMessageDialog(null, "Lo sentimos es la primera vez que entrara al sistema.\n"
-                + "Pongase en contacto con su DBA para que ingrese la contraseña del usuario \"System\" ...");
+                + "Pongase en contacto con su DBA para que ingrese la contraseña del usuario numero 1\"System\" ...");
         File file = new File("CrearUsuario.txt");
         usuario = "system";
         contrasena = JOptionPane.showInputDialog("Introduzca la contraseña del usuario system");
